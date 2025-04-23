c789879fc98bc398b4a89b2cf5bb3655fcb7fa03
iammoonman <420saas@gmail.com>
2025-04-23 10:14:40 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			pages: 'build/'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.nextTick.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
